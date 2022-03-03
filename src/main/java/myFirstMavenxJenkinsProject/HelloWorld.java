7cf9758118227bb7754dc3ea95a8706cf29e4e9b
AbhinavGits <abhinavshankar1897@gmail.com>
2022-03-03 16:34:14 +0530

    Committing after JenkinsGit 03/03/2022

--- src/main/java/myFirstMavenxJenkinsProject/HelloWorld.java
+++ src/main/java/myFirstMavenxJenkinsProject/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, this is AbhinavGits's repositry!!");
+		System.out.println("Changes successfully saved!");
 	}
 }

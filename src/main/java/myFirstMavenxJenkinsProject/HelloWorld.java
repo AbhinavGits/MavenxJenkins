34d070c3f4ae32d0a744a383e17b3f3b5d44f70b
AbhinavGits <abhinavshankar1897@gmail.com>
2022-03-03 14:45:18 +0530

    Committing after lunch 03/03/2022

---
+++ src/main/java/myFirstMavenxJenkinsProject/HelloWorld.java
@@ -0,0 +1,8 @@
+package myFirstMavenxJenkinsProject;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, this is AbhinavGits's repositry!!");
+	}
+}
